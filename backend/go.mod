83552846c15b860eec833107c59ce1dfaef21fa4
jere-oca <jere.oca@hotmail.com>
2025-09-08 16:20:54 -0300

    Refactor matrix processing endpoint

--- backend/go.mod
+++ backend/go.mod
@@ -2,7 +2,10 @@ module autosimplex
 
 go 1.25.0
 
-require github.com/gin-gonic/gin v1.10.1
+require (
+	github.com/gin-gonic/gin v1.10.1
+	gonum.org/v1/gonum v0.16.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
@@ -28,7 +31,7 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
-
-type MatrixRequest struct {
-	Matrix [][]float64 `json:"matrix"`
-}
 
 func main() {
 	r := gin.Default()
 
-	r.POST("/process", func(c *gin.Context) {
-		var req MatrixRequest
-		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		// Procesar 'req.Matrix'
-		c.JSON(http.StatusOK, gin.H{"matriz_recibida": req.Matrix})
-	})
+	r.POST("/process", process())
 
 	if err := r.Run(":8080"); err != nil {
 		panic("Error al iniciar el servidor: " + err.Error())
